31aca67e9f89d7df6dc4be2c20c3e75f29edcde0
carlaub <carlaurreablazquez@gmail.com>
2016-12-13 20:35:40 +0100

    listener search

--- js/logica.js
+++ js/logica.js
@@ -8,12 +8,47 @@
 
 (function() {
 
+    var Search = {
+        addListener: function() {
+            var button = document.getElementById("search-button");
+
+            Listener.add (button, "click", Listener.eventSearch, false);
+        }
+    }
 
 
-    var Application = {
+    var Listener = {
+        add: function(object, event, callback, capture) {
+            object.addEventListener(event,callback,capture);
+        },
+        eventSearch: function eventSearch (event) {
+            event.preventDefault();
+            var textField = document.getElementById("search-input");
+
+
+            if (textField.value.length === 0) {
+                alert("No has introducido ninguna busqueda.");
+            } else {
+                alert("hola, has introducido: " + textField.value);
+            }
+
+        }
 
     }
 
+    var Application = {
+        start: function() {
+            Search.addListener();
+        }
+    }
+
+    //Init application when DOM is loaded
+    Listener.add (
+        document,
+        "DOMContentLoaded",
+        Application.start(),
+        false
+    );
 
 
 
